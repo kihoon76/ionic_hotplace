780c551cdd1cbf7093734ff4f405254e941ada1d
hoon <lovedeer118@gmail.com>
2017-07-06 11:44:49 +0900

    20170706

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var naver;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+  map: any;
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ionViewDidLoad() {
+    this.loadMap();
+  }
+
+  loadMap() {
+    let mapOptions = {
+      center: new naver.maps.LatLng(37.3595704, 127.105399),
+      zoom: 10
+    };
+
+    this.map = new naver.maps.Map('map', mapOptions);
   }
 
 }
